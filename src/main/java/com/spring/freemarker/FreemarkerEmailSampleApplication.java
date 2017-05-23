c0645a4cfacc4636feeb1f3ff5ccc57e4addd4ec
Eric Anicet <anicetkeric@gmail.com>
2017-05-23 11:50:42 +0000

    update application.properties

--- src/main/java/com/spring/freemarker/FreemarkerEmailSampleApplication.java
+++ src/main/java/com/spring/freemarker/FreemarkerEmailSampleApplication.java
@@ -27,15 +27,15 @@ public class FreemarkerEmailSampleApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 
 		Mail mail = new Mail();
-		mail.setMailFrom("anicetkeric@gmail.com");
-		mail.setMailTo("anicetkeric@gmail.com");
+		mail.setMailFrom("anicetkeric@outlook.com");
+		mail.setMailTo("anicetkeric@outlook.com");
 		mail.setMailSubject("Spring Boot - Sending email with freemarker template");
 
 		Map<String, Object> model = new HashMap<String, Object>();
 		model.put("firstName", "David");
 		model.put("lastName", "Pham");
 		model.put("message", "message");
-		model.put("recipient", "anicetkeric@gmail.com");
+		model.put("recipient", "anicetkeric@outlook.com");
 		mail.setModel(model);
 
 		mailService.sendEmail(mail);
